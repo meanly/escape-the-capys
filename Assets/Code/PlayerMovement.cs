94d749c3f552cacb21103ad2dd3bd0cc836d91c6
meanly <63057881+meanly@users.noreply.github.com>
2024-11-17 12:15:03 +0800

    playermovement updated

--- Assets/Code/PlayerMovement.cs
+++ Assets/Code/PlayerMovement.cs
@@ -45,7 +45,7 @@ public class PlayerMovement : MonoBehaviour
                 body.velocity = Vector2.zero;
             }
             else
-                body.gravityScale = 7;
+                body.gravityScale = 1.2F;
 
             if (Input.GetKey(KeyCode.Space))
                 Jump();
